8838e068302f699448947d493c07ea0ba83572ad
nina <lui.n@northeastern.edu>
2024-08-12 06:56:00 -0700

    a6-1

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
+  cookie: {},
 };
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
@@ -38,10 +39,8 @@ if (process.env.NODE_ENV !== "development") {
     domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
-
 app.use(session(sessionOptions));
 app.use(express.json());
-
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
