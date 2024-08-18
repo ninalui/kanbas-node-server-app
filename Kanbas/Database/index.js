489a789ceee0a6045b406942e01905183202b06f
ninalui <144724444+ninalui@users.noreply.github.com>
2024-08-18 01:40:41 -0700

    update index.js

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -5,4 +5,4 @@ import users from "./users.js";
 import grades from "./grades.js";
 import enrollments from "./enrollments.js";
 import questions from "./questions.js";
-export default { courses, modules, assignments, users, grades, enrollments, quizzes, questions };
+export default { courses, modules, assignments, users, grades, enrollments, questions };
